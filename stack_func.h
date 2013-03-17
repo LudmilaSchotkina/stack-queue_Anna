7a98c67543720d7109653e5f59953f069a51bde0
annmasalska <mas.anna25@gmail.com>
2013-03-18 00:05:03 +0300

    Update stack_func.h

--- stack_func.h
+++ stack_func.h
@@ -1,5 +1,5 @@
-#ifndef QUEUE_FUNC_H
-#define QUEUE_FUNC_H
+#ifndef STACK_FUNC_H
+#define STACK_FUNC_H
 
 struct Node
 {
@@ -13,8 +13,8 @@ struct Stack
 bool push ( Stack &q, int dt);
 int pop(Stack &q);
 void clear(Stack &q);
-int onTop(Stack &q);
-bool isEmpty(Stack &q);
+int onTop(Stack q);
+bool isNonempty(Stack q);
 
 
 #endif
